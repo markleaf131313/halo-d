a584526d3ff6077ecd38641c67d5650076220af4
s <s@s.com>
2016-12-19 02:50:50 -0500

    Switch to compatibility GL context.

--- Impl/Source/App.d
+++ Impl/Source/App.d
@@ -182,7 +182,7 @@ void main(string[] args)
 
         enforce(SDL_Init(SDL_INIT_VIDEO) == 0);
 
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
 
