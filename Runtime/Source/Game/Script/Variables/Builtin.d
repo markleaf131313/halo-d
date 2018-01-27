372ec2c00d044b4ce2116949fa90279e65b1fb69
skl131313 <skl131313@users.noreply.github.com>
2018-01-27 14:38:18 -0500

    Backup minor work in Script variables.

---
+++ Runtime/Source/Game/Script/Variables/Builtin.d
@@ -0,0 +1,3 @@
+
+module Game.Script.Variables.Builtin;
+
---
+++ Runtime/Source/Game/Script/Variables/Meta.d
@@ -0,0 +1,11 @@
+
+module Game.Script.Variables.Meta;
+
+import Game.Script.Value;
+
+struct HsVariableMeta
+{
+    string name;
+    void* ptr;
+    HsType type;
+}
